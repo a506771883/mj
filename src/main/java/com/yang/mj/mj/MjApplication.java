6651f462af89660c0e7265bf8142c2ed268858ee
a506771883 <506771883@qq.com>
2019-11-23 16:21:17 +0800

    init repo

---
+++ src/main/java/com/yang/mj/mj/MjApplication.java
@@ -0,0 +1,13 @@
+package com.yang.mj.mj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MjApplication.class, args);
+    }
+
+}
