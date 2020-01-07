ba2ebb0e2db1333300e4afbb7da4a1d150e3cd86
yangping <yangping@ds.cn>
2020-01-07 08:49:09 +0800

    #完善部分

--- src/main/java/com/yang/mj/controller/publishController.java
+++ src/main/java/com/yang/mj/controller/publishController.java
@@ -32,6 +32,21 @@ public class publishController {
             HttpServletRequest request,
             Model model
     ){
+        if(title == null || title.equals("")){
+            model.addAttribute("error", "标题不能为空");
+            return "publish";
+        }
+        if(description == null || description.equals("")){
+            model.addAttribute("error", "问题补充不能为空");
+            return "publish";
+        }
+        if(tag == null || tag.equals("")){
+            model.addAttribute("error", "标签不能为空");
+            return "publish";
+        }
+        model.addAttribute("title",title);
+        model.addAttribute("description", description);
+        model.addAttribute("tag", tag);
 
         User user = null;
         Cookie[] cookies = request.getCookies();
